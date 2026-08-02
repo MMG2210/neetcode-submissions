15cdd1a025354c36c58bad6ed6ef2782678550c4
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2026-08-02 11:27:44 +0530

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length, dp[] = new int[n];
+        dp[0] = nums[0];
+
+        for(int i = 1; i < n; ++i){
+            if(i == 1){
+                dp[i] = Math.max(dp[i-1], nums[i]);
+            }
+            else dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
+        }
+
+        return dp[n-1];
+    }
+}
