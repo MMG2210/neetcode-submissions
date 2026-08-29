aed922992ce419ac0bb5271752ec0f63ded1d943
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2026-08-29 23:10:45 +0530

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rob_helper(self, nums: list) -> int:
+        prev, skip = 0, 0
+        for num in nums:
+            skip, prev = prev, max(prev, skip + num)
+        return prev
+    
+    def rob(self, nums: List[int]) -> int:
+        return max(self.rob_helper(nums[:len(nums)-1]), self.rob_helper(nums[1:])) if len(nums) > 1 else nums[0]
