34f96dfc51dfe98b62a49a77cac4d3bf87c9ced3
MMG2210 <101927391+MMG2210@users.noreply.github.com>
2026-08-30 21:48:10 +0530

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+
+        for coin in coins:
+            for val in range(coin, amount+1):
+                #
+                # t[i][j] = t[i-1][j] + t[i][j - coin] ie unbounded knapsack
+                # since we are only going to have one array, we have to compute this from coin -> amount
+                #
+                dp[val] += dp[val - coin]
+
+        print(dp)
+        return dp[amount]
